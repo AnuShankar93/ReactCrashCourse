bad91d830364837dc79ab7189585200c38c0771a
Anu Shankar <anushankardivakaran@gmail.com>
2018-04-19 12:24:20 +0530

    webpack 4 updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
 module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve('dist'),
+      filename: 'index_bundle.js'
+    },
     module: {
       rules: [
+        { test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" }
+          ]
+        },
         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          },
-          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
+        }, {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
       ]
     },
-    plugins: [htmlPlugin]
-  };
+    plugins: [HtmlWebpackPluginConfig]
+  }
